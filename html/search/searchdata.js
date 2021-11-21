a35d3b23a1eb51e2d5ad7e611a709e572acd5fe9
alu0101221960 <72342937+alu0101221960@users.noreply.github.com>
2021-11-21 20:33:05 +0000

    Add files via upload

---
+++ html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "begmpt",
+  1: "bempt",
+  2: "e",
+  3: "bgmp"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Clases",
+  2: "Archivos",
+  3: "Funciones"
+};
+
